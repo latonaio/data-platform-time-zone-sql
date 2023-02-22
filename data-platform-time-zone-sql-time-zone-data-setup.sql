973d1da77d112129225744499866fff900ab3421
MiyuYokoyama0109 <miyu.y@latona.io>
2023-02-22 13:39:10 +0900

    release

--- data-platform-time-zone-sql-time-zone-data-setup.sql
+++ data-platform-time-zone-sql-time-zone-data-setup.sql
@@ -2,19 +2,19 @@ LOCK TABLES `data_platform_time_zone_time_zone_data` WRITE;
 
 INSERT INTO `data_platform_time_zone_time_zone_data` (`TimeZone`)
 VALUES
-    ('UN');
-    ('FOB');
-    ('FH');
-    ('FCA');
-    ('FAS');
-    ('EXW');
-    ('DES');
-    ('DEQ');
-    ('DDU');
-    ('DDP');
-    ('DAF');
-    ('CPT');
-    ('CIP');
-    ('CIF');
+    ('UN'),
+    ('FOB'),
+    ('FH'),
+    ('FCA'),
+    ('FAS'),
+    ('EXW'),
+    ('DES'),
+    ('DEQ'),
+    ('DDU'),
+    ('DDP'),
+    ('DAF'),
+    ('CPT'),
+    ('CIP'),
+    ('CIF'),
     ('CFR');
 UNLOCK TABLES;
